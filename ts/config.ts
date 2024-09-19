e3dbd27d3e50de0b6b0d1e2114f36ce691bded82
Perfect03 <g.zlobin@ddirection.kz>
2024-09-19 17:23:25 +0500

    comments

--- ts/config.ts
+++ ts/config.ts
@@ -8,22 +8,25 @@ export const defaultHeaders = {} as any
 let onFetchFail: Function = () => undefined as object
 
 export const options = {
+    /**
+     * HTTP request configuration
+     */
     http: async (url: string, options: any, isblob: boolean) => {
-        let response = await fetch(url, options)
+        let response = await fetch(url, options) // standart fetch
         
-        let _meta = {
+        let _meta = { // object for meta data
             ok: response.ok,
             code: response.status,
             text: response.statusText,
         }
 
         if(!response.ok) {
-            const handleResponse = await onFetchFail(
+            const handleResponse = await onFetchFail( // use onFailure function if it were set by user
                 response.status,
                 () => options.http(url, options, isblob)
             )
 
-            if(typeof handleResponse === 'undefined' && !handleResponse?._meta?.ok) {
+            if(typeof handleResponse === 'undefined' && !handleResponse?._meta?.ok) { // if there were not onFailure function
                 return {
                     _meta,
                     header: response.headers,
@@ -34,7 +37,7 @@ export const options = {
                     }
                 }
             }
-            else {
+            else { // else set data from onFailure function; (and meta)
                 response = handleResponse
                 _meta = {
                     ok: response.ok,
@@ -44,20 +47,20 @@ export const options = {
             }
         }
 
-        if(isblob)
+        if(isblob) // for blob data
             return { header: response.headers, body: response.blob(), _meta }
         const raw = await response.text()
         
         if(raw.length > 0 && (raw[0] == '{' || raw[0] == '[')) {
             return { header: response.headers, body: JSON.parse(raw), _meta }
         }
-        return {header: response.headers, body: raw, _meta}
+        return {header: response.headers, body: raw, _meta} // this function returns RAW DATA (before template using)
     },
     cookie: { get: (name: string) => '', set: (name: string, value: any) => null as any} as any as { get(name: string): any, set(name: string, value: any): void },
     router: {} as any as { currentRoute: '', params: {}, query: {} },
-    _templates: {} as TemplateLogic,
+    _templates: {} as TemplateLogic, // incapsulated templates
     get templates() { return this._templates },
-    _isServer: false,
+    _isServer: false, // default - client
     get isServer() { return this._isServer },
     _apiRoot: '',
     get apiRoot() { return this._apiRoot }
@@ -67,23 +70,32 @@ export function onRecordFetchFailed(handle: Function) {
     onFetchFail = handle as any
 }
 
+/**
+ * User can set default header for all records
+ */
 export function setDefaultHeader(name: string, value: any) {
-    defaultHeaders[name] = value
+    defaultHeaders[name] = value // defaultHeaders are collecting to the array
 }
 
+/**
+ * User can set default auth value for all records
+ */
 export function setDefaultAuth(string: any) {
     defaultHeaders.Authorization = string
 }
 
+/**
+ * Returns converted data by template
+ */
 export function callPattern(name: string|Function, data: object) {
-    if(typeof name == 'string' && name in options.templates) {
+    if(typeof name == 'string' && name in options.templates) { //find template by name
         return options.templates[name](data)
     }
-    else if(typeof name == "function") {
+    else if(typeof name == "function") { // try use template if function is correct
         return name(data)
     }
     
-    return { data }
+    return { data } // if there isn't correct template
 }
 
 export function extendsPattern(parent: any, child: any) {
@@ -100,19 +112,25 @@ export function extendsPattern(parent: any, child: any) {
     return parent
 }
 
+/**
+ * Check for pattern (template) for Record
+ */
 function isValidPattern(pattern: string|Function) {
     if(typeof pattern === "string" && pattern.length > 0)
         return true
     else if(typeof pattern === "function")
         return true
-    else 
+    else // if pattern were not set or incorrect
         return false
 }
 
+/**
+ * Fetching data for store
+ */
 export async function storeFetch(url: string, requestInit: any, isblob: boolean, pattern: string|TemplateFunction): Promise<FetchResult> {
-    const response = await options.http(url, requestInit, isblob)
+    const response = await options.http(url, requestInit, isblob) // raw response
 
-    if(response instanceof Blob) {
+    if (response instanceof Blob) { // return value for Blob data
         return {
             header: response.header,
             data: response.body,
@@ -124,12 +142,12 @@ export async function storeFetch(url: string, requestInit: any, isblob: boolean,
         }
     }
 
-    if(typeof response.body == 'object' && !Array.isArray(response.body) && '_errorCode' in response.body) {
+    if (typeof response.body == 'object' && !Array.isArray(response.body) && '_errorCode' in response.body) { // case for error
         if(response.body._errorBody.length > 0 && response.body._errorBody[0] == '{') {
-            response.body._errorBody = JSON.parse(response.body._errorBody)
+            response.body._errorBody = JSON.parse(response.body._errorBody) // forming errorBody
         }
 
-        return {
+        return { // and return value with info about error IN DATA (that's very helpful)
             header: response.header,
             data: response.body._errorBody       || null,
             error: true,
@@ -144,15 +162,15 @@ export async function storeFetch(url: string, requestInit: any, isblob: boolean,
     let pageCount = 0
     let protocol = null
 
-    if(isValidPattern(pattern)) {
-        const result = callPattern(pattern, response.body) || {}
+    if (isValidPattern(pattern)) { // check template
+        const result = callPattern(pattern, response.body) || {} // use template for response.body if it's correct
 
         data      = result.data      ?? data
-        pageCount = result.pageCount ?? pageCount
+        pageCount = result.pageCount ?? pageCount // return data, pageCount variable and protocol data
         protocol  = result.protocol  ?? protocol
     }
 
-    return {
+    return { // return final correct value
         header: response.header,
         data,
         code: 200,
